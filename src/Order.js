3fa35e3173e4bf65ca1a3907ff7807101db554c3
Klis4 <kleshcenok.ilya@gmail.com>
2021-11-21 18:10:48 +0300

    feat: add express model

---
+++ src/Order.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const Order = new mongoose.Schema( {
+    name: {type: String, required: true},
+    phone: {type: String, required: true},
+    service: {type: String},
+  }
+
+);
+
+export default mongoose.model('Order', Order);
