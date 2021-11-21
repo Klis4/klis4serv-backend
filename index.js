03d9e1db6f780d4e3eae5d88f4f5a808e8ded772
Klis4 <kleshcenok.ilya@gmail.com>
2021-11-22 00:13:43 +0300

    add port generations for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './src/Router.js';
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
