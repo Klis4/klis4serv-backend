e89f3b1a71cc2615a7ebdb427543be797e3dbfcc
Klis4 <kleshcenok.ilya@gmail.com>
2021-11-21 21:45:45 +0300

    feat: add OrderService for work with DB

---
+++ src/OrderService.js
@@ -0,0 +1,42 @@
+import Order from './Order.js';
+
+class OrderService {
+  async create (order) {
+    const createdOrder = await Order.create(order);
+    return createdOrder;
+  }
+  
+  async getAll () {
+      const order = await Order.find();
+      return order;
+  }
+
+  async getOne (id) {
+      if (!id) {
+        throw new Error('ID is not defined');
+      }
+      const order = await Order.findById(id);
+      return order;
+  }
+
+  async update (order) {
+      if (!order._id) {
+        throw new Error('ID is not defined');
+      }
+      const updatedOrder = await Order.findByIdAndUpdate(order._id, order, {new: true});
+      return updatedOrder;
+  }
+
+  async delete (id) {
+      if (!id) {
+        throw new Error('ID is not defined');
+      }
+      const order = await Order.findByIdAndDelete(id);
+      return order;
+    
+     
+    
+  }
+}
+
+export default new OrderService();
