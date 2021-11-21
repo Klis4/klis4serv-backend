0df16ce3e5813606935e6836bceb6024a1d34c8e
Klis4 <kleshcenok.ilya@gmail.com>
2021-11-21 21:46:59 +0300

    feat: add controller for work with OrderService

---
+++ src/OrderController.js
@@ -0,0 +1,53 @@
+import Order from './Order.js';
+import OrderService from './OrderService.js';
+
+class OrderController {
+  async create (req, res) {
+    try {
+      const order = await OrderService.create(req.body);
+      res.json(order);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+  
+  async getAll (req, res) {
+    try {
+      const order = await OrderService.getAll();
+      return res.json(order);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+
+  async getOne (req, res) {
+    try {
+      const order = await OrderService.getOne(req.params.id);
+      return res.json(order);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+
+  async update (req, res) {
+    try {
+      const updatedOrder = await OrderService.update(req.body);
+      return res.json(updatedOrder);
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
+  }
+
+  async delete (req, res) {
+    try {
+      const order = await OrderService.delete(req.params.id);
+      return res.json(order);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+
+
+}
+
+export default new OrderController();
